c03167a69f9ffab128ffc3d09ae7cf9d0a89561e
Ray Shih <mnf.shih@gmail.com>
2012-10-20 20:51:29 +0800

    fix loading path problem

--- scripts/vmstartup/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-app_root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../")
-print app_root
-sys.path.append(app_root)
-
-from logger import logger
-
-logger.error('test')
