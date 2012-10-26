2142a93a1f1d8b03a3adea153fd9cc91ff36d17e
Ray Shih <mnf.shih@gmail.com>
2012-10-26 12:10:23 +0800

    fix test

--- fabfile.py
+++ fabfile.py
@@ -40,4 +40,4 @@ def test_ubuntu():
 
 def vm_ubuntu(command):
 	with cd(APP_ROOT):
-		run("sudo python royctl.py run vmstartup.vm_ubuntu_manager 'test(\"%(command)s\")'" % ({'command': command}))
+		run("sudo python royctl.py run vm_manager.vm_ubuntu_manager 'test(\"%(command)s\")'" % ({'command': command}))
