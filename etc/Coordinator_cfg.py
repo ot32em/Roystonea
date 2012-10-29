cfc704879c45118944adb56f5bcf48a5e0d340e7
Brian <billy170916@gmail.com>
2012-10-29 11:02:48 +0800

    prototype of portmapping

--- etc/Coordinator_cfg.py
+++ etc/Coordinator_cfg.py
@@ -7,8 +7,10 @@ else:
     db_host = 'localhost'
 db_account = 'root'
 db_password = '87888'
-db_name = 'roystonea_cluster'
+db_name = 'roystonea_roystonea'
 """ other config  """
 level = "coordinator"
 num_rthreads = 4
 FILENAME_MY_CONFIG = 'Coordinator_cfg'
+
+cmd_iptables = 'sudo iptables -t nat'
--- scripts/Coordinator.py
+++ scripts/Coordinator.py
@@ -6,6 +6,7 @@ ex. "python Coordinator.py 192.168.10.1 10000"
 ## You have to convert "HOST" to "int" type
 
 by Teddy, 2012/03/07
+'''
 
 from scripts.include.CommonHandler import  passArguments
 from scripts.include.Client import *
@@ -16,14 +17,11 @@ import MySQLdb as mdb
 import sys
 from scripts.include import CommonHandler, Message
 
-'''
-#from Roystonea.logger import logger
-# import os
 from logger import logger
-#TODO import pexpect
+import pexpect
 
 class Coordinator(CommonHandler):
-    from config.coordinatorConfig import *
+    from etc import Coordinator_cfg
     ''' custom init variable '''
     FILENAME_MY_CONFIG = FILENAME_MY_CONFIG
     num_rthreads = num_rthreads
@@ -45,6 +43,8 @@ class Coordinator(CommonHandler):
         self.db_account = db_account
         self.db_password = db_password
         self.db_name = db_name
+
+        self.cmd_iptables = cmd_iptables
    
     # receive msg from Rack to query imformation from database
     def RackAlgorithmResourceInformation(self, req):
@@ -149,15 +149,14 @@ class Coordinator(CommonHandler):
     
     def PortMapping(self, vmname, vmip, vmport, hostport, action):
         if action == 'a':
-            #TODO cmd_iptables
             iptables_cmd = '%s %s PREROUTING -p tcp --dport %s -j DNAT --to %s:%s' \
-                    %(cmd_iptables, '-A', hostport, vmip, vmport)
+                    %(self.cmd_iptables, '-A', hostport, vmip, vmport)
             logger.info('Add port mapping for %s, from %s to %s on hostmachine'%(vmname, vmport, hostport))
             logger.debug(iptables_cmd)
 
         elif action == 'd':
             iptables_cmd = '%s %s PREROUTING -p tcp --dport %s -j DNAT --to %s:%s' \
-                    %(cmd_iptables, '-D', hostport, vmip, vmport)
+                    %(self.cmd_iptables, '-D', hostport, vmip, vmport)
             logger.info('Delete port mapping for %s, from %s to %s on hostmachine'%(vmname, vmport, hostport))
             logger.debug(iptables_cmd)
         else :
@@ -172,45 +171,45 @@ class Coordinator(CommonHandler):
 
         return 1
 
-    # if connect:
-    #     log.info('Start.')
-    #     log.info('Database connected.')
-    #     while 1:
-    #         query_portreq = "SELECT * FROM %s WHERE state='adding' \
-    #                 OR state='deleting 'ORDER BY hostport"%(porttb)
-    #         db.query(query_portreq)
-    #         req_res = db.store_result()
-    #         fetched_req_data = req_res.fetch_row()
-    #         while fetched_req_data:
-    #             state = fetched_req_data[0][idx_state]
-    #             vmname = fetched_req_data[0][idx_vmname]
-    #             vmport = fetched_req_data[0][idx_vmport]
-    #             oldhostport = fetched_req_data[0][idx_hostport]
+        if connect:
+            log.info('Start.')
+            log.info('Database connected.')
+            while 1:
+                query_portreq = "SELECT * FROM %s WHERE state='adding' \
+                        OR state='deleting 'ORDER BY hostport"%(porttb)
+                db.query(query_portreq)
+                req_res = db.store_result()
+                fetched_req_data = req_res.fetch_row()
+                while fetched_req_data:
+                    state = fetched_req_data[0][idx_state]
+                    vmname = fetched_req_data[0][idx_vmname]
+                    vmport = fetched_req_data[0][idx_vmport]
+                    oldhostport = fetched_req_data[0][idx_hostport]
 
-    #             if (state == 'adding') :
-    #                 vmip = socket.gethostbyname(vmname)
-    #                 newport = oldhostport
-    #                 if (newport == '-1') :
-    #                     newport = get_port()
-    #                 if (newport != -1) :
-    #                     portmapping(vmname, vmip, vmport, newport, 'a')
-    #                     query = "UPDATE %s SET state='using', \
-    #                         hostport='%s', ip='%s' \
-    #                         WHERE hostport=%s"%(porttb, newport, vmip, oldhostport)
-    #                     db.query(query)
-    #                 else :
-    #                     log.error('No more port!')
-    #             elif (fetched_req_data[0][idx_state] == 'deleting') :
-    #                 #delete port
-    #                 vmip = fetched_req_data[0][idx_ip]
-    #                 portmapping(vmname, vmip, vmport, oldhostport, 'd')
-    #                 query = "DELETE FROM %s WHERE hostport=%s"%(porttb, oldhostport)
-    #                 db.query(query)
-    #             else :
-    #                 log.error("I don't know what's wrong!")
+                    if (state == 'adding') :
+                        vmip = socket.gethostbyname(vmname)
+                        newport = oldhostport
+                        if (newport == '-1') :
+                            newport = get_port()
+                        if (newport != -1) :
+                            portmapping(vmname, vmip, vmport, newport, 'a')
+                            query = "UPDATE %s SET state='using', \
+                                hostport='%s', ip='%s' \
+                                WHERE hostport=%s"%(porttb, newport, vmip, oldhostport)
+                            db.query(query)
+                        else :
+                            log.error('No more port!')
+                    elif (fetched_req_data[0][idx_state] == 'deleting') :
+                        #delete port
+                        vmip = fetched_req_data[0][idx_ip]
+                        portmapping(vmname, vmip, vmport, oldhostport, 'd')
+                        query = "DELETE FROM %s WHERE hostport=%s"%(porttb, oldhostport)
+                        db.query(query)
+                    else :
+                        log.error("I don't know what's wrong!")
 
-    #             fetched_req_data = req_res.fetch_row()
-    #         time.sleep(sleep_time)
+                fetched_req_data = req_res.fetch_row()
+            time.sleep(sleep_time)
     def sayHello(self):
         print 'Hello'
     
