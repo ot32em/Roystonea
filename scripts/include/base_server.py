dfd2dbe631f50a2b52ddb7e8b6f0255a307f38d4
Ray Shih <mnf.shih@gmail.com>
2012-11-06 23:53:10 +0800

    reconstruct base_server, success! yeah!

---
+++ scripts/include/base_server.py
@@ -0,0 +1,152 @@
+import SocketServer
+import pickle
+import threading
+import Message
+from time import sleep
+from thread_pool_mix_in import ThreadPoolTCPServer
+
+class BaseServer:
+    ''' Base Class for RPC
+
+    Usage:
+        rpc = BaseServer()
+        rpc.register_handle_function(name1, function_obj1)
+        rpc.register_handle_function(name2, function_obj2)
+        rpc.run()
+        rpc.shutdown()
+
+    '''
+
+    level = 'prototype'
+    shutdown_timeout = 5
+
+    def __init__(self, host, port):
+        self.host = host
+        self.port = int(port)
+        self.handle_functions = {}
+        self.server = None
+        self.living_threads = {}
+
+    def addr(self):
+        ''' Get address: (host, port) '''
+        return (self.host, int(self.port))
+
+    def run(self):
+        self.server = ThreadPoolTCPServer(self.addr(), self.createHandlerClass())
+        self.server.serve_forever()
+
+    def shutdown(self):
+        ''' Shutdown after all thread finish or timeout
+
+        Override the shutdown method from ThreadPoolMixIn
+
+        '''
+        timer = 0
+        isTimeout = False
+        while self.number_of_living_threads() > 0 :
+            sleep(1)
+            timer += 1
+            if timer > self.shutdown_timeout :
+                isTimeout = True
+                break
+            print("There still are %d threads not completed. Seconds remaining/timeout: (%d/%d) secs " %
+                    ( self.number_of_living_threads(), self.shutdown_timeout, self.shutdown_timeout-timer))
+            for t in self.living_threads.values() :
+                print("    Thread: %s" % t.name )
+        print("Shutting Down! is Timeout: %s" % isTimeout)
+
+        while self.server == None: pass # for server not start yet
+        self.server.shutdown()
+
+    def createHandlerClass(self):
+        ''' Create HandlerClass for handle request '''
+
+        class BaseServerHandler(SocketServer.BaseRequestHandler):
+            ''' This is the inner class for fit the SocketServer.BaseRequestHandler pattern '''
+
+            def handle(self):
+                ''' Handle function for TCPServer 
+
+                This function will do the following three steps:
+
+                    1 Collect all the data sended from client, 
+                    2 unpack it, 
+                    3 perform the correct function
+
+                While handle the request, it will record current thread,
+                    * This is for not to interrupt the execution
+
+                '''
+                self.master.living_threads[self.request] = threading.current_thread()
+
+                data = self._recv_data()
+                self._unpack_and_execute(data)
+
+                del self.master.living_threads[self.request]
+
+            def _recv_data(self):
+                data = ""
+                while True:
+                    raw = self.request.recv(1024)
+                    if not raw: break
+                    data += raw
+
+                return data
+            
+            def _unpack_and_execute(self, data):
+                message_name = "Undefined"
+                try: # try statement for converting serialized data to object-structure data
+                    message_name, ret = self.master.unpack_and_execute(data)
+                    self.request.send(pickle.dumps(ret))
+                
+                except pickle.UnpicklingError:
+                    error_req = Message.Error(msg='Unregonized Serial Data. Can not unpick it. Data Length: %d' % len(data) )
+                    self.request.send(pickle.dumps( error_req ) )
+
+                except KeyError:
+                    error_req = Message.Error(msg='Unacceptable Request Name(Not in DispatchDict). Request Name %s' % message_name )
+                    self.request.send(pickle.dumps( error_req ) )
+
+
+        BaseServerHandler.master = self # let handler access master
+
+        return BaseServerHandler
+
+    def unpack_and_execute(self, data):
+        ''' Use pickle.loads to unpack the data and send to registered handle function '''
+        recvobj = pickle.loads(data)
+
+        t = threading.current_thread()
+        ''' During processing the request,
+            it append current request information to threading name, 
+            then reset threading name after finish request '''
+        short_name = t.name
+        recvobj_class_name = recvobj.__class__.__name__
+        t.name = short_name+" handling {request_type}".format(request_type = recvobj_class_name)
+        return recvobj_class_name, self.handle_functions[recvobj_class_name](recvobj)  # processing request using functions binding in $dispatch_handlers
+
+    def register_handle_function(self, name, function):
+        '''
+        
+        Usage:
+        
+          def helloworld():
+              print("helloworld")
+        
+          rpcbase.register_handler_function("CmdTest", helloworld)
+        '''
+    
+        self.handle_functions[name] = function
+
+    def unregister_handle_function(self, name):
+        '''
+        Usage:
+        
+          rpcbase.unregister_handler_function("CmdTest")
+        '''
+
+        self.handle_functions.pop(name)
+
+    def number_of_living_threads(self):
+        return len(self.living_threads)
+
---
+++ test/test_include/test_base_server.py
@@ -0,0 +1,65 @@
+from Roystonea.scripts.include import Message
+from Roystonea.scripts.include.base_server import BaseServer
+from Roystonea.scripts.include import client
+import pickle
+import threading
+
+HOST, PORT = "localhost", 5000
+
+# handle functions for test
+def hello(data):
+    return "hello " + data.name
+
+def helloworld():
+    return "hello"
+
+def test_register_handle_funtion():
+    server = BaseServer(HOST, PORT)
+    server.register_handle_function("helloworld", helloworld)
+    assert server.handle_functions["helloworld"]() == "hello"
+
+def test_unregister_handle_function():
+    server = BaseServer(HOST, PORT)
+    server.register_handle_function("helloworld", helloworld)
+    assert server.handle_functions["helloworld"]() == "hello"
+
+    server.unregister_handle_function("helloworld")
+    assert server.handle_functions.has_key("helloworld") == False
+
+
+# setting for test unpack_and_execute
+class CmdHello:
+    def __init__(self, name):
+        self.name = name
+
+Message.CmdHello = CmdHello
+
+def test_unpack_and_execute():
+    cmd = CmdHello("world")
+    data = pickle.dumps(cmd)
+
+    server = BaseServer(HOST, PORT)
+    server.register_handle_function("CmdHello", hello)
+    message_name, ret = server.unpack_and_execute(data)
+
+    assert message_name == "CmdHello"
+    assert ret == "hello world"
+
+def test_base_server():
+
+    # server
+    server = BaseServer(HOST, PORT)
+    server.register_handle_function("CmdHello", hello)
+    def server_thread():
+        server.run()
+
+    t = threading.Thread(target = server_thread)
+    t.start()
+
+    # client
+    try:
+        ret = client.send_message((HOST, PORT), CmdHello("world"))
+        assert ret == "hello world"
+
+    finally:
+        server.shutdown()
