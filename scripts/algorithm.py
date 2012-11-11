485bbbd42649d0695b651989f92ba17997fad378
OT Chen <ot32em@gmail.com>
2012-11-11 17:01:39 +0800

    modified algorithm

--- scripts/algorithm.py
+++ scripts/algorithm.py
@@ -1,4 +1,29 @@
 from include.base_server import BaseServer
+# //
+# resourceList spec
+#    a list of resourceUnit
+# //
+# resourseUnit sepc
+#   type
+#   name 
+#   host
+#   port
+#   memory 
+#   disk
+#   minimalMemory
+#   minimalDisk (only exists in Rack,Cluster,Cloud )
+# //
+# vmInfo sepc
+#   at least config_memory, config_disk
+# //
+# algorithmReturn spec
+#   a list of resourceUnit with order
+# //
+# askRackReq( "vmInfo":
+#             "ParentUnit": Unit) 
+# //
+# askRackRes( "UnitList": a list of resourceUnits with order )
+
 
 class Algorithm(BaseServer):
 
@@ -20,40 +45,53 @@ class Algorithm(BaseServer):
         return self.cluster_addr
 
     def selectRackHandler(self, msg, client_address=None):
-        pass
 # ask coordinator for rack's resource
         racks = getRackResourcesFromCluster( client_address )
         vminfo = msg['vminfo']
         return self.rack_addr
+    def _getRackResourcesFromCluster( client_address ):
+        monitorAddress = self.monitor_addr
+        return
+
+
 
     def algorithmFirstFit(self, vmInfo, resourceList, params=dict() ):
-        priorityOrder = list()
-        for i in self._getRange( len( resourceInfoList), params ):
-            if self._enough( vmInfo, resourceInfoList[i] ) :
-                priorityOrder.append( i )
+        availiableList = self._filterAvalibaleResources( vmInfo, resourceList )
+        if self.isParamTrue( params, 'reverse' ) :
+            priorityOrder = sorted( availiableList, reverse=True )
         return priorityOrder
+
     def algorithmBestFit(self, vmInfo, resourceInfoList, params=dict()):
+        availiableList = self._filterAvalibaleResources( vmInfo, resourceList )
         factor = self._getFactor( params )
-        maxValue = -1
-        maxI = -1
-        pass
+        priorityOrder = sorted( availiableList, key=lambda resource: resource[factor], reverse=True ) # max put front
         
-
-    def avalibleResources(self, vmInfo, resourceInfoList, params ):
-        pass
-
+    def algorithmWorstFit(self, vmInfo, resourceInfoList, params=dict()):
+        availiableList = self._filterAvalibaleResources( vmInfo, resourceList )
+        factor = self._getFactor( params )
+        priorityOrder = sorted( availiableList, key=lambda resource: resource[factor] ) # max put back
         
+    def _isParamTrue( self, params, keyname ):
+        if params.has_key( keyname )  and params[ keyname ] == True :
+            return True
+        return False
+
+    def _filterAvailibleResources(self, vmInfo, resourceList, params ):
+        resultList = list()
+        for resourceInfo in resourceInfoList:
+            if resourceInfo.type = 'Node' :
+                resourceInfo['minimalMemory'] = resourceInfo['memory']
+                resourceInfo['minimalDisk'] = resourceInfo['disk']
+
+            if resourceInfo['minimalMemory']  > vmInfo['config_memory'] and 
+                resourceInfo['minimalDisk'] > vmInfo['config_disk']:
+                resultList.append( resourceInfo )
+        return resultList
         
     def _getFactor(self, params):
         if params.has_key('factor') and params['factor'] == 'disk' :
             return 'disk'
         return 'memory'
-
-
-    def _getRange(self, length, params ):
-        if params.has_key("reverse") and params['reverse'] == True:
-            return xrange( length - 1, 0 - 1, -1 )
-        return xrange( 0, length )
 
     def _enough(self, vmInfo, resourceInfo ):
         if vmInfo['config_memory'] > resourceInfo['memory'] and vmInfo['config_disk'] > resourceInfo['disk'] :
